f74ac56b51c5dd22292e44c14446d18791084d79
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-07-04 19:40:01 +0400

    Revert "fix all items in tab"
    This reverts commit 82abd47bda4a2777ea65a77e6d5f425ca7df6503.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Header from "./Header.jsx";
 
-import "./bootstrap.css";
-import "./reset.css";
-import "./index.css";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
