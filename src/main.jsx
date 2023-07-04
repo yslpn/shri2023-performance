82abd47bda4a2777ea65a77e6d5f425ca7df6503
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-07-04 19:36:07 +0400

    fix all items in tab

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Header from "./Header.jsx";
 
+import "./bootstrap.css";
+import "./reset.css";
+import "./index.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
