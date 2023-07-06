b1c36cf88ca45311aef3e7ae8e56c79890abd4fd
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-07-06 15:51:37 +0400

    cssInjectedByJsPlugin

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
-import { viteSingleFile } from "vite-plugin-singlefile";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), viteSingleFile(), createHtmlPlugin()],
+  plugins: [preact(), createHtmlPlugin(), cssInjectedByJsPlugin()],
   base: "/shri2023-performance/",
-  build: {
-    cssCodeSplit: false,
-  },
-  assetsInlineLimit: 100000000,
-  rollupOptions: {
-    output: {
-      manualChunks: () => "everything.js",
-    },
-  },
 });
