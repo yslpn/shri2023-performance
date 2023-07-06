f4bd9364d95f4414ed12600cd0c9db8426fe68c6
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-07-06 16:35:04 +0400

    Revert "single file for js css html"
    This reverts commit 459bf76ff48b1d8332cd7cb4fb31ffd6ed7fe98f.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
-import { viteSingleFile } from "vite-plugin-singlefile"
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), createHtmlPlugin(), viteSingleFile({ removeViteModuleLoader: true })],
+  plugins: [preact(), createHtmlPlugin(), cssInjectedByJsPlugin()],
   base: "/shri2023-performance/",
 });
