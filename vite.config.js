5b023f037c8d1f4d2d2cedc4b489a77d5015d793
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2023-07-06 15:41:41 +0400

    single file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { createHtmlPlugin } from "vite-plugin-html";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), viteSingleFile(), createHtmlPlugin()],
   base: "/shri2023-performance/",
+  build: {
+    cssCodeSplit: false,
+  },
+  assetsInlineLimit: 100000000,
+  rollupOptions: {
+    output: {
+      manualChunks: () => "everything.js",
+    },
+  },
 });
